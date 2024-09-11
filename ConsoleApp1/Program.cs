48d1a0de6753ce9b543689cc3c4a499617b6ab46
Abdullah Nasser <moustafa345nasse@gmail.com>
2024-09-11 22:36:16 +0300

    Projecta

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr =new int[10];
+
+            arr[0] = 100;
+            arr[^1] = 1000;
+
+            Console.WriteLine($"The number of first index = {arr[0]}");
+            Console.WriteLine($"The number of last index = {arr[^1]}");
+            Console.WriteLine($"The number of  index 4 = {arr[4]}");
+            
+        }
+    }
+}
+
